62b30fce6cdd72a311d240269bb0ddd858ec77c0
Craig Edwards <craigroe7@gmail.com>
2018-01-05 00:38:13 -0500

    binance test

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,12 +5,26 @@
           <router-link tag="md-button" to="/login" class="md-raised md-primary">Login</router-link>
           <router-link tag="md-button" to="/signup" class="md-raised md-accent">Sign Up</router-link>
         </md-layout>
-
-
     </div>
 </template>
 
 <script>
+const binance = require('node-binance-api');
+
+binance.websockets.trades(['BNBBTC', 'ETHBTC'], (trades) => {
+  const {
+    e: eventType,
+    E: eventTime,
+    s: symbol,
+    p: price,
+    q: quantity,
+    m: maker,
+    a: tradeId,
+  } = trades;
+  console.log(
+    `${symbol} trade update. price: ${price}, quantity: ${quantity}, maker: ${maker}`,
+  );
+});
 export default {
   name: 'home',
   data () {
